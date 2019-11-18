585223c631d9935f53c024eea6b57bdd291681be
ym3233@partner.kit.edu <ym3233@fh2n1992.localdomain>
2019-11-18 17:23:32 +0100

    Added parser.

--- AnneSim/get_nonconv_jobs_temp_analysis_DMRset.py
+++ AnneSim/get_nonconv_jobs_temp_analysis_DMRset.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 from SimAnalysis import CurrTempDMRset
 
 # This is an example on how to use the class CurrTempDMRset from SimAnalysis.
@@ -8,6 +9,21 @@ from SimAnalysis import CurrTempDMRset
 # with different DMRs). Should be used if one is interested in the influence 
 # of different DMRs on the J(T) curves and E_A vs. DMR.
 
+parser = argparse.ArgumentParser()
 
-dis_sim = CurrTempDMRset(rates="Marcus")
+parser.add_argument('-marcus', action='store_true', help='Specify if simulation was done with Marcus rates.')
+parser.add_argument('-Ecoul', action='store_true', help='Specify if sim. was done for a specific Ecoul.')
+
+args = parser.parse_args()
+print(args)
+
+if args.Ecoul:
+    rates = "Marcus"  # currently implemented that way in SimAnalysis
+else:
+    if args.marcus:
+        rates = "Marcus" 
+    else:
+        rates = "Miller"
+
+dis_sim = CurrTempDMRset(rates=rates, analyse_Ecoul=args.Ecoul)
 dis_sim.get_nonconv_joblist()
