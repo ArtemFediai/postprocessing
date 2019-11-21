3924286e9a9b563e7c4a9df88c544da911122227
ym3233@partner.kit.edu <ym3233@fh2n1995.localdomain>
2019-11-21 16:08:40 +0100

    Added scripts to analyse and restart E_A vs. sys. size simulations.

--- AnneSim/EA_vs_sys_size.py
+++ AnneSim/EA_vs_sys_size.py
@@ -2,6 +2,9 @@
 import argparse
 from SimAnalysis import CurrTempSimulation, CurrTempDMRset
 import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 
 
@@ -15,21 +18,47 @@ if args.marcus:
 else:
     rates = "Miller"
 
-sys_dir_unsorted = glob.glob('sys_*')
-list_sys_dirs = sorted(sys_dir_unsorted, key = lambda x: int(x.split('_')[-1]))
 
-for sys_dir in list_sys_dirs:
-    simulation = CurrTempSimulation(rates=rates,analyse_Ecoul=False, source_dir=sys_dir+'/',dest_dir='analysis/'+sys_dir)
 
-    simulation.collect_current_data()
+if not os.path.exists("analysis/Eact_vs_size.txt"):
+    sys_dir_unsorted = glob.glob('sys_*')
+    list_sys_dirs = sorted(sys_dir_unsorted, key = lambda x: int(x.split('_')[-1]))
+    n_sys = len(list_sys_dirs)
 
-    simulation.get_av_current()
-    simulation.plot_av_current(Tlim_low=250, Tlim_high=350,Jlim_low=10000,Jlim_high=40000,errorbar=True)
+    system_sizes = np.zeros(n_sys)
+    act_energy   = np.zeros(n_sys)
+    Tlow, Thigh  = 250, 350
+    for i_s,sys_dir in enumerate(list_sys_dirs):
+        simulation = CurrTempSimulation(rates=rates,analyse_Ecoul=False, source_dir=sys_dir+'/',dest_dir='analysis/'+sys_dir)
+        
+        simulation.get_act_energy(Tlim_low=Tlow,Tlim_high=Thigh)
 
-    simulation.get_av_conductivity()
-    simulation.plot_av_conductivity(errorbar=True)
+        simulation.plot_conv_analysis()
 
-    simulation.get_act_energy(Tlim_low=150,Tlim_high=300)
+        system_sizes[i_s] = simulation.sys_size
+        act_energy[i_s]   = simulation.act_energy
 
-    simulation.get_conv_analysis()
-    simulation.plot_conv_analysis()
+    with open("analysis/Eact_vs_size.txt",'w') as f:
+        if rates == "Marcus":   
+            sim_info = '# Field = {} V/nm, disorder = {} eV, DMR = {} %, lambda = {} eV\n'.format(simulation.field,simulation.dis,simulation.DMR, simulation.lam)
+        else:    
+            sim_info = '# Field = {} V/nm, disorder = {} eV, DMR = {} %\n'.format(simulation.field,simulation.dis,simulation.DMR)
+        f.write(sim_info)
+        f.write("# T. range for lin. fit of log J(1000/T): {} - {} K\n".format(Tlow,Thigh))    
+        f.write('#\n# size(nm)      Activation energy(meV)\n')
+        for i_s in range(n_sys):
+            f.write('{0:<8}   {1:<6}\n'.format(system_sizes[i_s],act_energy[i_s]))
+    f.close()
+else:
+    act_energy   = (np.loadtxt("analysis/Eact_vs_size.txt",comments='#',unpack=True))[1]
+    system_sizes = (np.loadtxt("analysis/Eact_vs_size.txt",comments='#',unpack=True))[0]
+    with open("analysis/Eact_vs_size.txt","r") as f:
+        sim_info = (f.readlines())[0]
+    sim_info = sim_info.replace('# ','')
+plt.plot(system_sizes,act_energy,marker="+", linestyle="None")
+plt.xlabel('System size (nm)')
+plt.xscale("log")
+plt.ylabel('$E_A$ (meV)')
+plt.title(sim_info,fontsize=14)
+plt.savefig("analysis/Eact_vs_site.png")
+plt.close()        
