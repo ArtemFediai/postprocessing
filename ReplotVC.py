f29348e8f81edb37b0308f373cc7f90eced6e551
ArtemFediai <artem.fedyay@gmail.com>
2020-02-13 12:08:02 +0100

    replot V_C added

---
+++ ReplotVC.py
@@ -0,0 +1,18 @@
+"""
+Test IP(R) as implemented by Timo
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import numpy as np
+import matplotlib
+matplotlib.use('pdf')
+import matplotlib.pyplot as plt
+import os
+import re
+import yaml
+import scipy.constants as const
+import time
+
+
+
