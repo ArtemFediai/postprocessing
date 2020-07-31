3a6b9641fc9774023f8725384018c59549dae1f5
Artem.Fediai <artem.fedyay@gmail.com>
2020-07-31 15:00:51 +0200

    compare_V4 improved

--- compare_V4.py
+++ compare_V4.py
@@ -9,7 +9,11 @@ as extracted from QP (damped shells)
 
 from __future__ import print_function
 from __future__ import absolute_import
+
+from typing import List
+
 from IP_or_EA_vs_R_ver_4 import QPOutput, Timer
+from IP_or_EA_vs_R_ver_4 import add_inverse_axis
 import numpy as np
 import matplotlib
 from numpy.core._multiarray_umath import ndarray
@@ -30,8 +34,15 @@ import gzip
 def main():
     with Timer() as t:
         folders = os.listdir('.')
-        folders = [folder for folder in folders if os.path.exists(folder+'/Analysis')]
+        folders: List[str] = [folder for folder in folders if os.path.exists(folder+'/Analysis')]
         A,B,C = [],[],[]
+        # compare_dict = {'dE0_cation': [], 'dE0_anion': [],
+        #                 'dV0_cation': [], 'dV0_anion': [],
+        #                 'dVe_cation': [], 'dVe_anion': [],
+        #                 'dEe_cation': [], 'dEe_anion': [],
+        #                 'list_radii': [], 'list_folders': []}
+        quick_dir = {}
+
         plt.figure()
         for i, f in enumerate(folders):
             os.chdir(f)
@@ -66,38 +77,68 @@ def main():
             os.chdir('../')
 
             #plt.plot(1./Analysis_output.inv_radii*10, Analysis_output.dEe_anion, label = f)
+
+            quick_dir[f] = Analysis_output
+
             A.append(Analysis_output.dEe_anion)
             B.append(Analysis_output.dVe_anion)
             C.append(Analysis_output.dV0_anion)
 
 #
         xlim = [0, 1.25]
-        for i,f in enumerate(folders):
-            plt.plot(Analysis_output.inv_radii*10, A[i], label=f)
+
+        for i, f in enumerate(folders):
+            r = quick_dir[f].inv_radii * 10
+            x = quick_dir[f]
+            sum = x.dEe_anion + x.dVe_anion + x.dV0_anion + x.dE0_anion
+            plt.plot(r, x.dEe_anion, color='C'+str(i))
+            plt.plot(r, x.dVe_anion, color='C'+str(i))
+            plt.plot(r, x.dV0_anion, color='C'+str(i))
+            plt.plot(r, x.dE0_anion*np.ones(len(r)), color='C'+str(i))
+            plt.plot(r, sum, linewidth='1.5', color='C'+str(i), label = f)
+
+        plt.legend(folders)
+
         plt.ylabel('dEe')
         plt.xlabel('1/r')
         plt.legend()
         plt.xlim(xlim)
-        plt.savefig('dEe.png', dpi=600)
+        #plt.grid()
+        ax1 = plt.gca()
+        add_inverse_axis(initial_axis=ax1)
+        #plt.grid()
+        plt.savefig('test.png', dpi=600)
         plt.close()
-#
-        for i,f in enumerate(folders):
-            plt.plot(Analysis_output.inv_radii*10, B[i], label=f)
-        plt.ylabel('dVe')
-        plt.xlabel('1/r')
-        plt.legend()
-        plt.xlim(xlim)
-        plt.savefig('dVe.png', dpi=600)
-        plt.close()
-#
-        for i,f in enumerate(folders):
-            plt.plot(Analysis_output.inv_radii*10, C[i], label=f)
-        plt.ylabel('dV0')
-        plt.xlabel('1/r')
-        plt.legend()
-        plt.xlim(xlim)
-        plt.savefig('dV0.png', dpi=600)
-        plt.close()
+
+
+
+
+#         for i,f in enumerate(folders):
+#             plt.plot(Analysis_output.inv_radii*10, A[i], label=f)
+#         plt.ylabel('dEe')
+#         plt.xlabel('1/r')
+#         plt.legend()
+#         plt.xlim(xlim)
+#         plt.savefig('dEe.png', dpi=600)
+#         plt.close()
+# #
+#         for i,f in enumerate(folders):
+#             plt.plot(Analysis_output.inv_radii*10, B[i], label=f)
+#         plt.ylabel('dVe')
+#         plt.xlabel('1/r')
+#         plt.legend()
+#         plt.xlim(xlim)
+#         plt.savefig('dVe.png', dpi=600)
+#         plt.close()
+# #
+#         for i,f in enumerate(folders):
+#             plt.plot(Analysis_output.inv_radii*10, C[i], label=f)
+#         plt.ylabel('dV0')
+#         plt.xlabel('1/r')
+#         plt.legend()
+#         plt.xlim(xlim)
+#         plt.savefig('dV0.png', dpi=600)
+#         plt.close()
 
     print("\nI am done")
     print("Total Computation Time: {} sec".format(t.interval))
