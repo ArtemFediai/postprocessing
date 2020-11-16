b0ac88c17c81b5b9ad76a563de1567deeda1e0cc
Artem.Fediai <artem.fedyay@gmail.com>
2020-11-16 08:59:24 +0100

    draft for the joint IPEACT_output

---
+++ joint_IPEACT_output_123.py
@@ -0,0 +1,21 @@
+# joint 1 2 3
+
+from __future__ import print_function
+from __future__ import absolute_import
+import numpy as np
+import matplotlib
+from numpy.core._multiarray_umath import ndarray
+import warnings
+import inspect
+import pickle
+
+matplotlib.use('pdf')
+import matplotlib.pyplot as plt
+import os
+import re
+import yaml
+import scipy.constants as const
+import time
+import gzip
+
+
