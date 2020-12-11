11efeb6e90963ca453fbe15c2429efab17df7569
ArtemFediai <artem.fedyay@gmail.com>
2020-12-11 19:40:36 +0100

    two extra plots for polarization componentwise

--- eps_paper/components_polarization_energy.py
+++ eps_paper/components_polarization_energy.py
@@ -70,28 +70,42 @@ def main():
 
         delta_delta_anion = np.array(jonas_dict['delta_delta_E0_anion_mean'])
         delta_delta_cation = np.array(jonas_dict['delta_delta_E0_cation_mean'])
+        delta_delta = 0.5 * (delta_delta_cation - delta_delta_anion)
 
         p_av = (e0i_anion + eii_anion + v0i_anion + e0i_cation + eii_cation + v0i_cation)/2.0
-        de0i = 0.5*(e0i_anion+e0i_cation) #dE_env
+        deii = 0.5*(e0i_anion+e0i_cation) #dE_env
         dv0i = 0.5*(v0i_anion+v0i_cation) #dR_core
-        deii = 0.5*(eii_anion+eii_cation) #dV_env
+        dvii = 0.5*(eii_anion+eii_cation) #dV_env
+        de0 = 0.5*(e0_anion + e0_cation)
 
-        point_a, point_b, point_b1, target_i, point_a1, epsilon = eps_mod(folder, radii, radii_not_renormalized, p_av, [20, 40], [1, 1E5])
+        bulks = []
 
+        #total ->
+        point_a, point_b, point_b1, target_i, point_a1, epsilon = eps_mod(folder, radii, radii_not_renormalized, p_av, [20, 50], [1, 1E5])
+        plt.plot([point_a[0], point_b[0]], [point_a[1], point_b[1]], LineStyle='dotted', color='black')
+        plt.plot(point_a[0], point_a[1], LineStyle='dotted', marker='o', color='black')
+        #<- total
+
+        #partial ->
         array_of_ps_at_0 = []
-        array_of_ps = [de0i, dv0i, deii, p_av]
+        array_of_ps = [deii, dv0i, dvii] # various polarizations
         for i, p in enumerate(array_of_ps):
             point_a, point_b, point_b1, _, point_a1, _ = eps_mod(folder, radii, radii_not_renormalized, p, [20, 40], [1, 1E5])
             print([point_a1[1]])
             array_of_ps_at_0.append(point_a1[1])
-            plt.plot([point_a[0], point_b[0]], [point_a[1], point_b[1]], LineStyle='dashed' , marker='o')
+            plt.plot([point_a[0], point_b[0]], [point_a[1], point_b[1]], LineStyle='dotted', color=f'C{i}')
+            plt.plot([point_a[0]], [point_a[1]], LineStyle='dashed', marker='o', color=f'C{i}')
+            bulks.append(point_a[1])
+
+        bulks.append(delta_delta)##dd
+
         print(array_of_ps_at_0)
+        #<- partial
 
-        plt.plot(10*radii**(-1), de0i , label='$\Delta E_{env}$') #
-        plt.plot(10*radii**(-1), dv0i, label='$\Delta V_{core}$') #
-        plt.plot(10*radii**(-1), deii, label='$\Delta V_{env}$') #
-        plt.plot(10*radii**(-1), p_av, label= '$P_{env}$', color='black') # all points
-
+        plt.plot(10*radii**(-1), deii, label='$\Delta E_\mathrm{env}$', color='C0')#
+        plt.plot(10*radii**(-1), dv0i, label='$\Delta V_\mathrm{core}$', color='C1') #
+        plt.plot(10*radii**(-1), dvii, label='$\Delta V_\mathrm{env}$', color='C2') #
+        plt.plot(10*radii**(-1), p_av, label= '$\mathrm{P}_\mathrm{env}$', color='black')  # all points
 
         eps_range = np.zeros(n)
         for i in range(n):
@@ -102,33 +116,68 @@ def main():
         print('eps_mean ' + folder + '= ', eps_mean)
         print('eps_std ' + folder + '= ', eps_std)
 
-        plt.xlim([0, 2])
-        plt.ylim([-1, 2])
-        plt.grid()
-        plt.ylabel('Components of the polarization energy, eV')
-        plt.xlabel('$10/R,  10 / \AA^{-1} $')
+        plt.xlim([-0.1, 2])
+        plt.ylim([-1, 2.5])
+        plt.ylabel('Components of $\mathrm{P}$, eV')
+        plt.xlabel('$10/R,  10 / mathrm{\AA}^{-1} $')
         if radii_renormalization:
-            plt.xlabel('Inverse renormalized radius $10/R,  10 / \AA^{-1} $')
+            plt.xlabel('Inverse renormalized radius $10/R,  10 / \mathrm{\AA}^{-1} $')
         else:
-            plt.xlabel('Inverse radius $10/R, \AA^{-1} $')
+            plt.xlabel('Inverse polarization radius $10/R, \mathrm{\AA}^{-1} $')
         plt.legend()
         ax1 = plt.gca()
-        add_inverse_axis(ax1, rs_plot=np.array([5, 7, 10, 20, 30, 50]))
-        plt.text(0.1, 1.6, folder, fontsize=12, bbox={'facecolor': 'white', 'edgecolor': 'grey', 'alpha': 0.5, 'pad':10})
+        add_inverse_axis(ax1, rs_plot=np.array([5, 7, 10, 20, 30, 50]), x_label='$R, \mathrm{\AA}$')
+
+        plt.grid()
+        plt.text(0.6, 2.15, folder, fontsize=12, bbox={'facecolor': 'None', 'edgecolor': 'None', 'alpha': 0.5, 'pad':10})
         plt.savefig('p_average_componentwise_{}.png'.format(folder), dpi=600, bbox_inches='tight')
+        plt.savefig('p_average_componentwise_{}.svg'.format(folder), bbox_inches='tight')
         plt.close()
 
-        #seaborn
 
-        fig, ax = plt.subplots( )
-        values =  np.transpose([0.5*(e0i_anion+e0i_cation), 0.5*(v0i_anion+v0i_cation), 0.5*(eii_anion+eii_cation), p_av])
-        data = pd.DataFrame(values, 10/radii_not_renormalized, columns=["delta_e_env", "delta_v_core", "delta_v_env", "p"])
-        sns.lineplot(data=data, palette='tab10', linewidth=2.5, ax=ax)
-        add_inverse_axis(ax, rs_plot=np.array([5, 7, 10, 20, 30, 50]))
-        plt.xlim([0, 2])
-        plt.ylim([-1, 2])
-        plt.savefig('p_av_components_seaborn_{}.png'.format(folder))
+        #<-- pie chart
+        data = {'$\Delta E_\mathrm{env}$': abs(bulks[0]),
+                '$\Delta V_\mathrm{core}$': abs(bulks[1]),
+                '$\Delta V_\mathrm{env}$': abs(bulks[2]),
+                '$\Delta\Delta E_0$': abs(bulks[3])}
+
+        # data = {'$\Delta E_\mathrm{env}$': abs(bulks[0]),
+        #         '$\Delta V_\mathrm{core}$': abs(bulks[1]),
+        #         '$\Delta V_\mathrm{env}$': abs(bulks[2])}
+
+        pie, ax = plt.subplots(figsize=[3.5, 4.5])
+        labels = data.keys()
+        plt.pie(x=list(data.values()), autopct="%.1f%%", explode=[0.05] * len(data), labels=labels, pctdistance=0.5)
+        plt.title(folder, fontsize=14)
+        pie.savefig(f"PieChart_{folder}.png", dpi=600)
+        pie.savefig(f"PieChart_{folder}.svg")
         plt.close()
+
+        #--> bar chart
+        data_signed = \
+                {'$\Delta V_\mathrm{core}$': (bulks[1]),
+                '$+\Delta E_\mathrm{env}$': (bulks[0]),
+                '$+\Delta V_\mathrm{env}$': (bulks[2]),
+                '$+\Delta\Delta E_0$': (bulks[3])}
+
+        data_cumulative = {}
+        data_numpy = np.array([x for x in data_signed.values()])
+        data_str = [x for x in data_signed.keys()]
+        data_cumulative = data_numpy.cumsum()
+
+        # data_for_panda = [[x,y] for x,y in zip(data_str, data_numpy)]
+        data_for_panda = [[x,y] for x,y in zip(data_str, data_cumulative)]
+        df = pd.DataFrame(data=data_for_panda, columns=["Cumulative sum", "Polarization"])
+        ax = sns.barplot(data=df, x='Cumulative sum', y='Polarization', palette='Blues_d')
+        ax.set(ylabel="Cumulative polarization, eV")
+        ax.set(xlabel="Polarization components")
+        ax.set(ylim=[0, 3])
+        plt.title(folder, fontsize=14)
+        plt.savefig(f'bar_plot_{folder}.png', dpi=600)
+        plt.savefig(f'bar_plot_{folder}.svg')
+        plt.close()
+        #<-- bar chart
+
 
 def eps(folder, radii, energies, ab,a1b1):
     c = 7.1997176734999995  # coefficient in the formula
