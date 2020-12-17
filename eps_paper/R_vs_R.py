d2c25de47f9d3c1d8dec3e6599132a862a7c53b5
ArtemFediai <artem.fedyay@gmail.com>
2020-12-17 20:55:18 +0100

    R vs R bugs fixed

--- eps_paper/R_vs_R.py
+++ eps_paper/R_vs_R.py
@@ -22,7 +22,6 @@ def main():
 
     folders = ['C60' ,'aNPD' , 'TCTA']
     folders_names = ['C60' ,'NPB' , 'TCTA']
-    radii_not_renormalized = np.loadtxt(folders[0] + '/Analysis/p_elementwise/radii.dat')
 
 
     # for average eps -->
@@ -30,10 +29,6 @@ def main():
     lower_limits_eps = np.linspace(a, a+c, n)
     upper_limits_eps = np.linspace(b, b+c, n)
 
-    # <-- for average eps
-    number_density_nm3_aNPD = 1.158623303810171  # aNPD
-    number_density_nm3_TCTA = 0.9162165860886232  # TCTA
-    number_density_nm3_C60 = 1.4266923  # C60
 
     plt.figure('R vs R')
 
@@ -41,47 +36,34 @@ def main():
     name_of_pcs_pcs_folder = 'pcs_pcs'
     name_of_pcs_qp_folder = 'pcs_qp'
 
-    if not os.path.exists(name_of_postprocessing_folder):
-        os.mkdir(name_of_postprocessing_folder)
-        if not os.path.exists(name_of_postprocessing_folder + '/' + name_of_pcs_qp_folder):
-            os.mkdir(name_of_postprocessing_folder + '/' + name_of_pcs_qp_folder)
-    else:
-        pass
-
+    p_av_for_all = []
+    radii_renormalized_all = []
     ab = [0, 2]
     plt.plot(ab, ab, color='grey', linestyle=':', label='homogenious material')
     for i, folder in enumerate(folders):
-        radii = np.loadtxt(folder + '/Analysis/p_elementwise/radii_renormalized.dat')
-        e0i_anion = np.loadtxt(folder + '/Analysis/p_elementwise/dEe_anion.dat')  #DFT depol
-        eii_anion = np.loadtxt(folder + '/Analysis/p_elementwise/dV0_anion.dat')  #class pol
-        v0i_anion = np.loadtxt(folder + '/Analysis/p_elementwise/dVe_anion.dat')  #class depol
-        e0i_cation = np.loadtxt(folder + '/Analysis/p_elementwise/dEe_cation.dat')  #DFT depol
-        eii_cation = np.loadtxt(folder + '/Analysis/p_elementwise/dV0_cation.dat')  #class pol
-        v0i_cation = np.loadtxt(folder + '/Analysis/p_elementwise/dVe_cation.dat')  #class depol
+        with open(folder+'/jonas_dict.yaml') as fid:
+            jonas_dict = yaml.load(fid, Loader=yaml.FullLoader)
+        radii_not_renormalized = np.array(jonas_dict['R_direct_grid'])
+        radii_renormalized = np.array(jonas_dict['r_renormalized'])
+
+        e0i = np.array(jonas_dict['E0_dft'])
+        v0i = np.array(jonas_dict['E0_sd'])
+        eii = np.array(jonas_dict['E0_env_env'])
+
+        e0i_anion = e0i - np.array(jonas_dict['E_anion_dft'])
+        v0i_anion = v0i - np.array(jonas_dict['E_anion_sd'])
+        eii_anion = eii - np.array(jonas_dict['E_anion_env_env'])
+
+        e0i_cation = -np.array(jonas_dict['E_cation_dft']) + e0i
+        v0i_cation = -np.array(jonas_dict['E_cation_sd']) + v0i
+        eii_cation = -np.array(jonas_dict['E_cation_env_env']) + eii
+
 
         p_av = (e0i_anion + eii_anion + v0i_anion + e0i_cation + eii_cation + v0i_cation)/2.0
+        p_av_for_all.append(p_av)
+        radii_renormalized_all.append(radii_renormalized)
+        plt.plot(10*radii_not_renormalized**(-1), 10*radii_renormalized**(-1), label=folders_names[i], marker='o', markerfacecolor='None', color='C{}'.format(i))
 
-        point_a, point_b, point_b1, target_i = eps(folder, radii, p_av, [20, 40], [50, 1E100])
-
-        #plt.plot([point_a[0], point_b1[0]], [point_a[1],point_b1[1]], color='black')
-        #plt.plot(10*radii[target_i]**(-1), p_av[target_i], 'o', color='black', mfc='none')
-        #plt.plot(10*radii**(-1), p_av, label=folder)
-
-
-        plt.plot(10*radii_not_renormalized**-1, 10*radii**-1, label=folders_names[i], marker='o', markerfacecolor='None')
-
-        #save in postprocessing folder
-        #np.savetxt(name_of_postprocessing_folder + '/' + name_of_pcs_qp_folder + '/' + 'radii.dat', radii_not_renormalized)
-        #np.savetxt(name_of_postprocessing_folder + '/' + name_of_pcs_qp_folder + '/' + 'p_av_{}.dat'.format(folder), p_av)
-
-        #eps_range = np.zeros(n)
-        #for i in range(n):
-        #    eps_range[i] = eps_only(folder, radii, p_av, [lower_limits_eps[i], upper_limits_eps[i]])
-        #eps_std = np.std(eps_range)
-        #eps_mean = np.mean(eps_range)
-
-        #print('eps_mean ' + folder + '= ', eps_mean)
-        #print('eps_std ' + folder + '= ', eps_std)
 
     plt.xlim([0, 2])
     plt.ylim([0, 2])
@@ -95,16 +77,54 @@ def main():
     plt.gca().set_aspect('equal', adjustable='box')
     plt.savefig('R_vs_R.png', dpi=600)
     plt.savefig('R_vs_R.svg')
-
-
-    plt.draw()
     plt.close()
 
-    #
-    #ax, smth = plt.subplots()
-    #ax.plot(radii, radii_not_renormalized)
+    # pol_vs_R and pol_vs_R
 
-    #
+    fig, axes = plt.subplots(1,2, figsize=[10,5])
+    ax = axes[1]
+    ax0 = axes[0]
+
+    ax0.plot(ab, ab, color='grey', linestyle=':', label='homogenious material')
+    for ii, folder_name in enumerate(folders_names):
+
+        point_a, point_b, point_b1, target_i, point_a1, epsilon = eps_mod(folders[ii], radii_renormalized_all[ii], radii_not_renormalized, p_av_for_all[ii], [20, 40], [1, 1E5])
+        ax.plot([point_a1[0], point_b1[0]], [point_a1[1], point_b1[1]], color='black')
+        ax.plot(10*radii_renormalized_all[ii]**(-1.0), p_av_for_all[ii], linestyle='solid', label='$\mathrm{P}$' + '$(R)$'.format(folder_name) + ' for ' + folder_name, color='C{}'.format(ii))
+        ax.plot(10*radii_not_renormalized**(-1.0), p_av_for_all[ii], linestyle='--', label= '$\mathrm{P}$' + r'$(\tilde{R})$' + ' for ' + folder_name, color='C{}'.format(ii), alpha=0.5, linewidth='3')
+
+        ax0.plot(10*radii_not_renormalized**(-1), 10*radii_renormalized_all[ii]**(-1), label=folders_names[ii], markerfacecolor='None', color='C{}'.format(ii))
+
+
+    #ax
+    ax.set_xlabel(r'$10/R$, $10/ \tilde{R}, \mathrm{\AA}^{-1}$')
+    ax.set_ylabel('Average polarization energy $\mathrm{P}$, eV')
+    ax.set_xlim([0,1])
+    ax.set_ylim([0.0,1.0])
+    ax.legend()
+    add_inverse_axis(ax)
+
+
+    #ax0
+
+    ax0.set_xlim([0, 2])
+    ax0.set_ylim([0, 2])
+    ax0.grid()
+    ax0.set_xlabel('$10/R, \mathrm{\AA}^{-1}$')
+    ax0.set_ylabel(r'10/ $\tilde{R},  \mathrm{\AA}^{-1} $')
+    ax0.legend()
+    #plt.tight_layout()
+    # ax1 = plt.gca()
+    # add_inverse_axis(ax1)
+    ax0.set_aspect('equal', adjustable='box')
+
+    # savefig
+    fig.tight_layout()
+    fig.savefig('P_vs_R_vs_R.png', dpi=600)
+    fig.savefig('P_vs_R_vs_R.svg')
+
+
+
 
 
 
@@ -149,5 +169,35 @@ def eps_only(folder, radii, energies, ab):
     epsilon = c / (c + coef_poly[0])
     return epsilon
 
+def eps_mod(folder, radii, r_not_renormalized, energies, ab,a1b1):
+    # differs from eps by outputting another points
+    c = 7.1997176734999995  # coefficient in the formula
+
+    a, b, a1, b1 = ab[0], ab[1], a1b1[0], a1b1[1]  # analyze interval; plot interval
+
+    target_i = np.where(np.logical_and(r_not_renormalized >= a, r_not_renormalized <= b))[0]
+    # target_i1 = np.where(np.logical_and(radii > a1, radii < b1))[0]
+    selected_r = radii[target_i]  # selected interval to compute epsilon
+    # selected_r1 = radii[target_i1]
+    selected_energies = energies[target_i]
+    # selected_energies1 = energies[target_i1]
+
+    coef_poly = np.polyfit(1.0 / selected_r, selected_energies, 1)
+
+    selected_fitted_energies = coef_poly[0] / selected_r + coef_poly[1]
+
+    epsilon = c / (c + coef_poly[0])
+
+    print( "eps " + folder + " = ", epsilon)
+
+    e_a1 = coef_poly[0]/a1 + coef_poly[1]
+    e_b1 = coef_poly[0]/b1 + coef_poly[1]
+
+    point_a = [10 / b1, coef_poly[1]]
+    point_b = [10 / selected_r[-1], selected_energies[-1]]
+    point_b1 = [10 / a1, e_a1]
+    point_a1 = [10 / b1, e_b1]
+
+    return point_a, point_b, point_b1, target_i, point_a1, epsilon
 if __name__ == '__main__':
     main()
